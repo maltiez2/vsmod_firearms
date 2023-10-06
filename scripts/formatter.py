3805a487326cea1ae2787b8630b798553ebe684f
maltiez2 <maltiez2@gmail.com>
2023-10-06 18:02:01 +0300

    Add scripts to format jsons

---
+++ scripts/formatter.py
@@ -0,0 +1,35 @@
+import pythonmonkey as pm
+import json
+import argparse
+import os
+from glob import iglob
+
+def format(path):
+    formatter = pm.require('./formatter')
+    f = open(path, "r", encoding='utf-8-sig')
+    data = json.load(f);
+    content = formatter.stringify(json.dumps(data), {"maxLength": length})
+    f.close()
+    f = open(path, "w", encoding='utf-8-sig')
+    f.write(content)
+    f.close()
+
+parser = argparse.ArgumentParser(description="Requires 'pythonmonkey'")
+parser.add_argument("-f", "--file", default="input.json", help="path to file")
+parser.add_argument("-l", "--length", default=200, help="max width")
+parser.add_argument("-a", "--all", action="store_true", help="format all recursively")
+parser.add_argument("-d", "--directory", default="itemtypes", help="look for specific folder in the path")
+args = parser.parse_args()
+file = args.file
+length = args.length
+directory = args.directory
+
+if (args.all):
+    rootdir_glob = f'../resources/**/{directory}/**/*.json'
+    file_list = [f for f in iglob(rootdir_glob, recursive=True) if os.path.isfile(f)]
+    for f in file_list:
+        print(f"formatting: {f}")
+        format(f)
+else:
+    print(f"formatting: {file}")
+    format(file)
